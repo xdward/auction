1bfbfd843d2029bd7f3b065a0b501b12214061de
xdward <78007088+xdward@users.noreply.github.com>
2026-07-25 00:46:51 -0400

    move consumer factories

--- internal/service/consumers.go
+++ internal/service/consumers.go
@@ -12,6 +12,55 @@ import (
 	"github.com/xdward/auction/internal/service/db"
 )
 
+// NewQueueSubscriber registers a new subscriber to a queue. A single queue subscriber will
+// receive messages for the subject it is subscribed to. Multiple queue subscribers with the same
+// subject and queue form a queue group. Messages sent to the queue group’s subject are delivered to
+// exactly one subscriber, which is randomly chosen within the group.
+func NewQueueSubscriber(
+	w *Worker,
+	natsAddr string,
+	redisAddr string,
+	subj string,
+	queue string,
+	cb nats.MsgHandler,
+) {
+	slog.SetLogLoggerLevel(slog.LevelDebug)
+
+	nc, err := nats.Connect(natsAddr)
+	if err != nil {
+		panic(err)
+	}
+	defer nc.Drain()
+
+	js, err := jetstream.New(nc)
+	if err != nil {
+		panic(err)
+	}
+
+	db := db.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: "", // no password parameter
+		DB:       0,  // use default db
+	})
+	defer db.Close()
+
+	w.NATS = nc
+	w.JS = js
+	w.DB = db
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	_, err = w.NATS.QueueSubscribe(subj, queue, cb)
+	if err != nil {
+		panic(err)
+	}
+
+	slog.Info("watching", "queue", queue, "subject", subj)
+	<-ctx.Done()
+	slog.Info("shutting down")
+}
+
 // NewScheduleConsumer attaches a new consumer to a stream. The first invocation of this function
 // creates a new stream with the AllowMsgSchedules flag enabled. A new durable pull consumer is also
 // created, which the worker uses to handle messages. To schedule a message for future delivery,
