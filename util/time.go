5636c4daaf05ea35fd95983c939c68fdc9cafaef
xdward <78007088+xdward@users.noreply.github.com>
2026-07-26 21:25:09 -0400

    fix time integer overflow

--- util/time.go
+++ util/time.go
@@ -1,10 +1,24 @@
 package util
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
+const maxMS = uint64(math.MaxInt64) / uint64(time.Millisecond)
+
+// DurationTimestamps returns (start, end), where start is the current time and end is the start
+// time plus ms milliseconds. Both timestamps are in UTC.
 func DurationTimestamps(ms uint64) (time.Time, time.Time) {
 	start := time.Now().UTC()
-	duration := time.Duration(ms) * time.Millisecond
+
+	var duration time.Duration
+	if ms > maxMS {
+		duration = time.Duration(math.MaxInt64) // clamp
+	} else {
+		duration = time.Duration(ms) * time.Millisecond
+	}
+
 	end := start.Add(duration)
 
 	return start, end
